9eb99b03f216019a98df8ef44aee9821a8f9c809
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-02-01 23:54:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,48 +7,56 @@ const socket = require('socket.io');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
-app.use(cors());
 
-//server connection
+// Update CORS settings to allow requests from your frontend deployed on Vercel
+app.use(cors({
+  origin: 'https://cv-project1-6ypa.vercel.app',  // Your frontend URL
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
-const server = app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`)
-})
+// Server connection
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-//SOCKET.IO INTEGRATION
-
-const io =  socket(server, {
-        cors: {
-            origin: 'http://localhost:3000',
-            methods: ['GET', 'POST'],
-        },
-    });
+// SOCKET.IO INTEGRATION
+const io = socket(server, {
+  cors: {
+    origin: 'https://cv-project1-6ypa.vercel.app', // Update origin to frontend URL
+    methods: ['GET', 'POST'],
+  },
+});
 
 global.onlineUsers = new Map();
 
 io.on('connection', (socket) => {
-    global.chatSocket = socket;
+  global.chatSocket = socket;
 
-    socket.on('add-user', (userId) => {
-        onlineUsers.set(userId, socket.id);
-    });
-    socket.on("send-msg",async(data) => {
-        const sendUserSocket = await onlineUsers.get(data.to);
-        if (sendUserSocket) {
-            socket.to(sendUserSocket).emit('msg-recieve', data.message);
-        }
-    });
+  socket.on('add-user', (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on('send-msg', async (data) => {
+    const sendUserSocket = await onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('msg-recieve', data.message);
+    }
+  });
 });
 
-//database connection
-
-mongoose.connect("mongodb+srv://harish:hamarihk@cluster0.zmgaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
-    console.log("connected to the database");
-})
+// Database connection
+mongoose.connect("mongodb+srv://harish:hamarihk@cluster0.zmgaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+  });
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/auth', require('./ROUTES/authRoute'));
-app.use('/user',require('./ROUTES/userRoute'));
-app.use('/update',require('./ROUTES/updateRoute'));
+app.use('/user', require('./ROUTES/userRoute'));
+app.use('/update', require('./ROUTES/updateRoute'));
 app.use('/messages', require('./ROUTES/messageRoute'));
